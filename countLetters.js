770b24dad9aad80af5fa41a311c9edec7a20e7a5
Riley Fournier <rileyjfournier@gmail.com>
2021-02-03 23:02:05 +0000

    counting letters function, returning a new object with the amount of letters within string

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let newObj = {};
+  for (const letter of str) {
+    if (letter !== " ") {
+      if (newObj[letter]) {
+        newObj[letter] += 1;
+      } else {
+        newObj[letter] = 1;
+      }
+    }
+  }
+  console.log(newObj);
+};
+
+strExample = "hello my name is riley";
+
+countLetters(strExample);
+
+assertEqual(strExample, "hello my name is riley");
