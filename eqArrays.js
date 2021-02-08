2f25fd06820bebee07fc41755c1349cf1ebb6de7
Riley Fournier <rileyjfournier@gmail.com>
2021-02-08 20:32:44 +0000

    creating test files for eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let eqChecker = 0;
   for (let i = 0; i < arr1.length; i++) {
@@ -20,6 +12,4 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+module.exports = eqArrays;
